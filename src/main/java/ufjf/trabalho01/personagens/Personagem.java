5e3415c671f6ce4da2fb4303e2e1f08a15371c25
Miguel <migueltrrds@gmail.com>
2025-06-04 17:06:12 -0300

    Adicionado função para renderizar personagens na tabela

--- src/main/java/ufjf/trabalho01/personagens/Personagem.java
+++ src/main/java/ufjf/trabalho01/personagens/Personagem.java
@@ -1,5 +1,10 @@
 package ufjf.trabalho01.personagens;
 
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import ufjf.trabalho01.GridManager;
+
 public abstract class Personagem {
     protected String nome;
     protected int pontosDeVida = 100;
@@ -9,22 +14,30 @@ public abstract class Personagem {
     protected int posicaoX;
     protected int posicaoY;
     protected String tipo;
-
+    protected Node view;
 
     public Personagem(String tipo, String nome, int forcaDeAtaque,
                      int forcaDeDefesa, int alcanceDeAtaque) {
+        this.tipo = tipo;
         this.nome = nome;
         this.forcaDeAtaque = forcaDeAtaque;
         this.forcaDeDefesa = forcaDeDefesa;
         this.alcanceDeAtaque = alcanceDeAtaque;
+        this.posicaoY = -1;
+        this.posicaoX = -1;
+        this.view = criarView();
     }
 
-    
-     public abstract void usarPoderEspecial(Personagem oponente);
-     public String getTipo() {
+    public String getTipo() {
          return this.tipo;
-     }
+    }
 
+    protected Node criarView() {
+        Circle circle = new Circle(16);
+        circle.setFill(Color.RED);
+        circle.setStroke(Color.BLACK);
+        return circle;
+    }
 
     public void mover(int novaX, int novaY) {
         this.posicaoX = novaX;
@@ -38,12 +51,13 @@ public abstract class Personagem {
         }
     }
 
-
     public int calcularDistancia(Personagem outro) {
         int diffX = Math.abs(this.posicaoX - outro.getPosicaoX());
         int diffY = Math.abs(this.posicaoY - outro.getPosicaoY());
         return Math.max(diffX, diffY);
     }
+
+    public abstract void usarPoderEspecial(Personagem oponente);
 
     // Getters e Setters
     public String getNome() {
@@ -86,5 +100,7 @@ public abstract class Personagem {
         this.alcanceDeAtaque = alcance;
     }
 
-
+    public Node getView() {
+        return this.view;
+    }
 }
